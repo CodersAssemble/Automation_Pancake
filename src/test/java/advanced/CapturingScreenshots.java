8b531cc75688715a5ae08dd64c1f39bdd952502a
CodersAssemble <yaseswiniy@gmail.com>
2024-01-04 10:07:33 +0530

    Capturing Screenshots Code..

---
+++ src/test/java/advanced/CapturingScreenshots.java
@@ -0,0 +1,39 @@
+package advanced;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CapturingScreenshots {
+
+	public static void main(String[] args) throws InterruptedException{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demoqa.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+		
+		 File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		 
+		 try {
+			 FileUtils.copyFile(screenshotFile, new File("F://CodeBlooded/automationPanCake/screenshots/ss.png"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		 
+		 Thread.sleep(3000);
+		 driver.quit();
+
+	}
+
+}
